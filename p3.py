8ac0a2ed6e923c09c8dd114f13bf032d78a3f039
umamalam <155060609+umamalam@users.noreply.github.com>
2024-09-12 11:33:08 +0530

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,45 @@
+Given a string s, find the length of the longest 
+substring
+without repeating characters.
+
+ 
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+
+Constraints:
+
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s)==0:
+            return 0
+        map={}
+        max_length=start=0
+        for i in range(len(s)):
+            if s[i] in map and start<=map[s[i]]:
+                start=map[s[i]] +1
+            else:
+                max_length = max(max_length,i-start + 1)
+            map[s[i]]=i
+        return(max_length)            
+
+     
